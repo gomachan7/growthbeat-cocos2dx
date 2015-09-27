0cb63ed91ce603aef1810b07af8b8e98f0d1ccc0
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-09-27 17:08:26 +0900

    add Growth Push Delegate

--- source/Classes/GrowthAnalytics/ios/GrowthAnalyticsCCInternal.h
+++ source/Classes/GrowthAnalytics/ios/GrowthAnalyticsCCInternal.h
@@ -21,7 +21,7 @@
 + (void)track:(NSString *)eventId properties:(NSDictionary *)properties;
 + (void)track:(NSString *)eventId option:(int)option;
 + (void)track:(NSString *)eventId properties:(NSDictionary *)properties option:(int)option;
-+ (void)track:(NSString *)_namespace name:(NSString *)name properties:(NSDictionary *)properties option:(int)option;
++ (void)track:(NSString *)_namespace name:(NSString *)eventId properties:(NSDictionary *)properties option:(int)option;
 
 + (void)tag:(NSString *)tagId;
 + (void)tag:(NSString *)tagId value:(NSString *)value;
