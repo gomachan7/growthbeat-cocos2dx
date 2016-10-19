d57f3239c55144573ce7f8f2026544f34503d6fe
Takabe Takuto <takabe_takuto@cyberagent.co.jp>
2016-10-19 15:52:01 +0900

    Fix to avoid hiding a member of subclass

--- source/proj.android/src/com/growthpush/Cocos2dxReceiverService.java
+++ source/proj.android/src/com/growthpush/Cocos2dxReceiverService.java
@@ -4,8 +4,6 @@ import com.growthpush.bridge.ExternalFrameworkReceiverService;
 
 public class Cocos2dxReceiverService extends ExternalFrameworkReceiverService {
 
-    private com.growthpush.Cocos2dxBridge bridge;
-
     public Cocos2dxReceiverService() {
         super();
         this.bridge = new com.growthpush.Cocos2dxBridge();
